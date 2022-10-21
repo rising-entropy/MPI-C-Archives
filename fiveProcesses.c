4efe0631f3b8a10ed4a3105f07dbb42a27e6e293
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-10-21 18:42:51 +0530

    5 processes

--- fiveProcesses.c
+++ fiveProcesses.c
@@ -7,12 +7,8 @@ int main(int argc, char** argv){
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Comm);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
-
-    // Get the group of processes in MPI_COMM_WORLD
-    MPI_Group world_group;
-    MPI_Comm_group(MPI_COMM_WORLD, &world_group);
     
-    printf("Process Rank -  %d of %d and Group - %d\n", process_Rank, size_Of_Comm, world_group.id);
+    printf("Process Rank -  %d of %d", process_Rank, size_Of_Comm, world_group.id);
 
     MPI_Finalize();
     return 0;
