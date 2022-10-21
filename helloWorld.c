bc71e8d5fbd92337dd128d696fb9db2561385a70
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-10-21 17:15:24 +0530

    code

--- helloWorld.c
+++ helloWorld.c
@@ -8,7 +8,7 @@ int main(int argc, char** argv){
     MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Comm);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
 
-    printf("Hello World! - Process %d of %d\n", process_Rank, size_Of_Cluster);
+    printf("Hello World! - Process %d of %d\n", process_Rank, size_Of_Comm);
 
     MPI_Finalize();
     return 0;
