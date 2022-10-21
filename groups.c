ac3ba9b58a377eee8a3a4d46d5ed13bc5e672bae
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-10-21 18:33:57 +0530

    groups

--- groups.c
+++ groups.c
@@ -1,20 +1,30 @@
-// Get the rank and size in the original communicator
-int world_rank, world_size;
-MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+#include <stdio.h>
+#include <mpi.h>
 
-int color = world_rank / 4; // Determine color based on row
+int main(int argc, char** argv){
 
-// Split the communicator based on the color and use the
-// original rank for ordering
-MPI_Comm row_comm;
-MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &row_comm);
+    MPI_Init(&argc, &argv);
+    // Get the rank and size in the original communicator
+    int world_rank, world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-int row_rank, row_size;
-MPI_Comm_rank(row_comm, &row_rank);
-MPI_Comm_size(row_comm, &row_size);
+    int color = world_rank / 4; // Determine color based on row
 
-printf("WORLD RANK/SIZE: %d/%d \t ROW RANK/SIZE: %d/%d\n",
-	world_rank, world_size, row_rank, row_size);
+    // Split the communicator based on the color and use the
+    // original rank for ordering
+    MPI_Comm row_comm;
+    MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &row_comm);
 
-MPI_Comm_free(&row_comm);
+    int row_rank, row_size;
+    MPI_Comm_rank(row_comm, &row_rank);
+    MPI_Comm_size(row_comm, &row_size);
+
+    printf("WORLD RANK/SIZE: %d/%d \t ROW RANK/SIZE: %d/%d\n",
+        world_rank, world_size, row_rank, row_size);
+
+    MPI_Comm_free(&row_comm);
+    MPI_Finalize();
+    return 0;
+}
+
