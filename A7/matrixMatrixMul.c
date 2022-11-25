db59386185953be310c06956e58899342ec669b6
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-11-25 21:03:19 +0530

    mat mul

--- A7/matrixMatrixMul.c
+++ A7/matrixMatrixMul.c
@@ -1,18 +1,32 @@
-// O(n^2) Matrix Multiplication (Using N processors)
-
+/*
+ * There are some simplifications here. The main one is that matrices B and C 
+ * are fully allocated everywhere, even though only a portion of them is 
+ * used by each processor (except for processor 0)
+ */
 #include <mpi.h>
 #include <stdio.h>
 
-#define SIZE 12			/* Size of matrices */
+#define SIZE 1024			/* Size of matrices */
 
 int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
 
-void print_matrix(int m[SIZE][SIZE], int n)
+void fill_matrix(int m[SIZE][SIZE])
+{
+  static int n=1;
+  int i, j;
+  for (i=0; i<SIZE; i++)
+    for (j=0; j<SIZE; j++)
+      m[i][j] = n++;
+}
+
+
+
+void print_matrix(int m[SIZE][SIZE])
 {
   int i, j = 0;
-  for (i=0; i<n; i++) {
+  for (i=0; i<SIZE; i++) {
     printf("\n\t| ");
-    for (j=0; j<n; j++)
+    for (j=0; j<SIZE; j++)
       printf("%2d ", m[i][j]);
     printf("|");
   }
@@ -21,7 +35,7 @@ void print_matrix(int m[SIZE][SIZE], int n)
 
 int main(int argc, char *argv[])
 {
-  int myrank, P, from, to, i, j, k, n, from1, to1;
+  int myrank, P, from, to, i, j, k;
   int tag = 666;		/* any value will do */
   MPI_Status status;
   
@@ -29,61 +43,57 @@ int main(int argc, char *argv[])
   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);	/* who am i */
   MPI_Comm_size(MPI_COMM_WORLD, &P); /* number of processors */
 
-  if (myrank==0){
-	  printf("Enter N : ");
-	  scanf("%d",&n);
-  }
-  MPI_Bcast (&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
   /* Just to use the simple variants of MPI_Gather and MPI_Scatter we */
   /* impose that SIZE is divisible by P. By using the vector versions, */
   /* (MPI_Gatherv and MPI_Scatterv) it is easy to drop this restriction. */
 
-  if (P != n) {
-    if (myrank==0) printf("Number of processors not divisible by matrix size.\n");
+  if (SIZE%P!=0) {
+    if (myrank==0) printf("Matrix size not divisible by number of processors\n");
     MPI_Finalize();
     exit(-1);
   }
 
-  from = myrank * (n*n)/P;
-  to = (myrank+1) * (n*n)/P;
+  from = myrank * SIZE/P;
+  to = (myrank+1) * SIZE/P;
 
   /* Process 0 fills the input matrices and broadcasts them to the rest */
   /* (actually, only the relevant stripe of A is sent to each process) */
 
   if (myrank==0) {
-  	printf("Fill Matrix A : \n");
-    for (i=0; i<n; i++)
-    	for (j=0; j<n; j++)
-       		scanf("%d",&A[i][j]);
-    printf("Fill Matrix B : \n");
-    for (i=0; i<n; i++)
-    	for (j=0; j<n; j++)
-       		scanf("%d",&B[i][j]);	
+    fill_matrix(A);
+    fill_matrix(B);
   }
 
-  MPI_Bcast (B, n*n, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Scatter (A, n*n/P, MPI_INT, A[from], n*n/P, MPI_INT, 0, MPI_COMM_WORLD);
+  double start = MPI_Wtime();
 
-  printf("computing slice %d (from element %d to %d)\n", myrank, from+1, to);
+  MPI_Bcast (B, SIZE*SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Scatter (A, SIZE*SIZE/P, MPI_INT, A[from], SIZE*SIZE/P, MPI_INT, 0, MPI_COMM_WORLD);
+
+  printf("computing slice %d (from row %d to %d)\n", myrank, from, to-1);
   for (i=from; i<to; i++) 
-    for (j=n; j<n; j++) {
+    for (j=0; j<SIZE; j++) {
       C[i][j]=0;
-      for (k=0; k<n; k++)
-		C[i][j] += A[i][k]*B[k][j];
+      for (k=0; k<SIZE; k++)
+	    C[i][j] += A[i][k]*B[k][j];
     }
 
-  MPI_Gather (C[from], n*n/P, MPI_INT, C, n*n/P, MPI_INT, 0, MPI_COMM_WORLD);
-
+  MPI_Gather (C[from], SIZE*SIZE/P, MPI_INT, C, SIZE*SIZE/P, MPI_INT, 0, MPI_COMM_WORLD);
   if (myrank==0) {
-    printf("\n\n");
-    print_matrix(A,n);
-    printf("\n\n\t       * \n");
-    print_matrix(B,n);
-    printf("\n\n\t       = \n");
-    print_matrix(C,n);
-    printf("\n\n");
+    double finish = MPI_Wtime();
+
+    // printf("\n\n");
+    // print_matrix(A);
+    // printf("\n\n\t       * \n");
+    // print_matrix(B);
+    // printf("\n\n\t       = \n");
+    // print_matrix(C);
+    // printf("\n\n");
+
+    printf("Exection Time: %f\n", finish - start);
   }
 
   MPI_Finalize();
   return 0;
 }
+
+
