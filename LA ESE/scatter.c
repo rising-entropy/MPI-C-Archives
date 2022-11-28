550add642ecff9f30f61d9d006e1e29d52b0331f
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-11-28 13:56:31 +0530

    scatter

--- LA ESE/scatter.c
+++ LA ESE/scatter.c
@@ -10,6 +10,6 @@ int main(int argc, char** argv){
     int world_rank, world_size;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
+    printf("Hello!");
     MPI_Finalize();
 }
