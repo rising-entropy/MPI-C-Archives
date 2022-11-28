1f9a118da49f1b21473e0388fe7a35346111123c
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-11-28 12:09:08 +0530

    mpi hello

---
+++ practice/helloWorld.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<mpi.h>
+
+int main(int argc, char** argv){
+    int process_rank, comm_size;
+    MPI_INIT(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
+    printf("%d / %d", process_rank, comm_size);
+    MPI_Finalize();
+    return 0;
+}
